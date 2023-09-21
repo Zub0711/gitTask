1a15ad37145fb3520ad5a837bf3d753800440b5b
Zubair Hoosain <zubairhoosain@hotmail.com>
2023-09-21 17:06:52 +0200

    The program will now take the users name and log it

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,6 @@
-// Compulsory Task 1
+// Compulsory Task 1 & 2
 
 // console.log to  display git is awesome
-console.log("Git is Awesome");
+let userInput = prompt("Please enter your name");
+
+console.log(`Hi ${userInput}`);
